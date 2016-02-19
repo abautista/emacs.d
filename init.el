a08113ff65261a905e4cf5b6d69616431e364f32
Alberto Bautista <alberto.bautista@me.com>
2016-02-19 20:37:07 +0100

    Current config

--- init.el
+++ init.el
@@ -1,64 +1,102 @@
-;;; init.el --- emacs init file.
-
-;;; Commentary:
-
-;; This is my emacs configuration.
-
-;;; Code:
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+                         ("org" . "http://orgmode.org/elpa/")))
 
-;; initialize the packages and create the packages list if not exists
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; disable auto-save and auto-backup
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-(setq scroll-conservatively most-positive-fixnum)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; clean the interface
+;;;;
+;; Packages I use
+;;;;
+
+(use-package paredit
+  :ensure t)
+
+(use-package deft
+  :ensure t
+  :config
+  (setq deft-default-extension "org"))
+
+(use-package caroline-theme
+  :ensure t)
+
+(use-package company
+  :ensure t
+  :init
+  (setq company-minimum-prefix-length 3)
+  (setq company-idle-delay 0.2)
+  :config
+  (global-company-mode))
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-global-mode))
+
+(use-package yasnippet
+  :ensure t
+  :init
+  (yas-global-mode 1)
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends")
+  (defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+	backend
+      (append (if (consp backend) backend (list backend))
+	      '(:with company-yasnippet))))
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  :config
+  (setq yas-snippet-dirs '("~/.emacs.d/snippets")))
+
+;;;;
+;; Customizations
+;;;;
+
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(load-theme 'tango-dark)
+(menu-bar-mode -1)
+(ido-mode t)
+(setq frame-resize-pixelwise t)
 
-;; disable auto-save and auto-backup
-(setq auto-save-default nil)
-(setq make-backup-files nil)
+(global-set-key (kbd "C-x C-g") 'deft)
 
-;; (setq explicit-shell-file-name "/bin/zsh")
-(setq system-uses-terminfo nil)
+(custom-set-faces
+ '(default ((t
+	     (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 105 :width normal)))))
 
-;; org-mode
-(add-hook 'text-mode-hook
-	  '(lambda ()
-	     (set-fill-column 80)
-	     (turn-on-auto-fill)))
+;; Org mode customizations
 
-;;(global-set-key (kbd "C-c o") (lambda ()
-;;				(interactive)
-;;				(find-file "~/notes/organizer.org")))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
 
-(setq org-default-notes-file "~/notes/organizer.org")
+(setq org-directory "~/.org")
+(setq org-default-notes-file "~/.org/refile.org")
 
-(setq org-capture-templates
-      '(
-	("j" "Journal Entry"
-	 entry (file+datetree "~/notes/journal.org")
-	 "* Event: %?\n\n %i\n\n From: %a"
-	 :empty-lines 1)
-))
-			      
-			  
+(setq org-agenda-files (list
+			"~/.org/todo.org"
+			"~/.org/tutorials.org"
+			"~/.org/courses.org"
+			"~/.org/books.org"
+			"~/.org/english.org"
+			"~/.org/handmade.org"))
 
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "M-o") 'other-window)
+;;;;
+;; Language specific
+;;;;
 
-;; packages-configuration
-(load "~/.emacs.d/packages-configuration.el")
+;; For editing c/c++
+(defun abautista-c-mode-common-hook()
+  (setq tab-width 4
+	indent-tabs-mode nil)
+  (setq c-basic-offset 4))
 
-;;; init.el ends here
+(add-hook 'c-mode-common-hook 'abautista-c-mode-common-hook)
